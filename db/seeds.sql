e34578dd705e3f29be771d2e96b749c77451b863
Darren Steverson <dcs194@scarletmail.rutgers.edu>
2019-07-28 17:58:38 -0400

    data for database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black Bean Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Breakfast Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Wild Salmon Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Portobello Mushroom Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", FALSE);
